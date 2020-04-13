f61ff9cdb3dae919d777c2485f54a3abdbe16b54
unknown <henriquedelgadosales@gmail.com>
2020-04-13 16:55:19 -0300

    heroku log

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const Discord = require("discord.js");
 const { prefix, token } = require("./config.json");
 
+console.log("token: " + token);
+
 const client = new Discord.Client();
 
 client.once("ready", () => {
@@ -20,14 +22,14 @@ function play(id) {
         .replace(/[\u0300-\u036f]/g, "");
       fs.renameSync(`./audio/${f}`, `./audio/${newFile}`);
     }); */
-  let avaible_sentences = fs.readdirSync("./audio").map((f) => {
+  let avaible_sentences = fs.readdirSync("./audio").map(f => {
     return f
       .slice(0, -4)
       .toLowerCase()
       .normalize("NFD")
       .replace(/[\u0300-\u036f]/g, "");
   });
-  let sentence = avaible_sentences.filter((s) => s.includes(file));
+  let sentence = avaible_sentences.filter(s => s.includes(file));
   if (!sentence.length) {
     server.queue.shift();
     return;
@@ -37,7 +39,7 @@ function play(id) {
   let voiceChannel = message.member.voice.channel;
   return voiceChannel
     .join()
-    .then((connection) => {
+    .then(connection => {
       if (!server.playing) {
         let dispatcher = connection.play(`./audio/${sentence}.mp3`);
         dispatcher.on("start", () => {
@@ -54,7 +56,7 @@ function play(id) {
         });
       }
     })
-    .catch((err) => {
+    .catch(err => {
       message.channel.send(err);
       console.log(err);
       voiceChannel.leave();
@@ -62,7 +64,7 @@ function play(id) {
 }
 
 function list(message) {
-  let avaible_sentences = fs.readdirSync("./audio").map((f) => {
+  let avaible_sentences = fs.readdirSync("./audio").map(f => {
     return f
       .slice(0, -4)
       .toLowerCase()
@@ -72,7 +74,7 @@ function list(message) {
   message.channel.send(avaible_sentences.join("\n"));
 }
 
-client.on("message", (message) => {
+client.on("message", message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
   // LIST COMMAND
   if (message.content.slice(1).startsWith("list")) {
